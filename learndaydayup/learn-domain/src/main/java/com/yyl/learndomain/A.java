324f81292dc7d77916e04e3ac075659680ed3503
yuanyunlu <39354624+yuanyunlu@users.noreply.github.com>
2019-05-15 22:45:53 +0800

    Update A.java

--- learndaydayup/learn-domain/src/main/java/com/yyl/learndomain/A.java
+++ learndaydayup/learn-domain/src/main/java/com/yyl/learndomain/A.java
@@ -2,6 +2,6 @@ package com.yyl.learndomain;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("增加main方法");
+        System.out.println("增加main方法1");
     }
 }
