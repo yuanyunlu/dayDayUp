33391c5dcf18bd44ebd28b6fbcb6bfd611edf66f
yuanyunlu <1115919794@qq.com>
2019-05-15 23:04:55 +0800

    增加main方法111branch1

--- learndaydayup/learn-domain/src/main/java/com/yyl/learndomain/A.java
+++ learndaydayup/learn-domain/src/main/java/com/yyl/learndomain/A.java
@@ -2,6 +2,6 @@ package com.yyl.learndomain;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("增加main方法111");
+        System.out.println("增加main方法111branch1");
     }
 }
