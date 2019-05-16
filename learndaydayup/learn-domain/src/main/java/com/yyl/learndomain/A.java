349b645027e749a7ddca230191ab3fd7ac065338
yuanyunlu <1115919794@qq.com>
2019-05-16 14:44:59 +0800

    ATagV1.0.1

--- learndaydayup/learn-domain/src/main/java/com/yyl/learndomain/A.java
+++ learndaydayup/learn-domain/src/main/java/com/yyl/learndomain/A.java
@@ -2,6 +2,6 @@ package com.yyl.learndomain;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("A_tagv1.0.0");
+        System.out.println("A_tagv1.0.1");
     }
 }
