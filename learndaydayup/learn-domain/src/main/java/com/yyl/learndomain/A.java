ffec8325a703cb01e2bdb090e9df82f466e5ad57
yuanyunlu <1115919794@qq.com>
2019-05-16 16:17:55 +0800

    两个A1

--- learndaydayup/learn-domain/src/main/java/com/yyl/learndomain/A.java
+++ learndaydayup/learn-domain/src/main/java/com/yyl/learndomain/A.java
@@ -2,6 +2,7 @@ package com.yyl.learndomain;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("A2");
+        System.out.println("A1");
+        System.out.println("A1");
     }
 }
