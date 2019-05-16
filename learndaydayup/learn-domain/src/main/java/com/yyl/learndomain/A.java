20d4131da5ec4f18aa9a1572b324a654faa4a6c7
yuanyunlu <1115919794@qq.com>
2019-05-16 17:37:37 +0800

    A

--- learndaydayup/learn-domain/src/main/java/com/yyl/learndomain/A.java
+++ learndaydayup/learn-domain/src/main/java/com/yyl/learndomain/A.java
@@ -3,5 +3,10 @@ package com.yyl.learndomain;
 public class A {
     public static void main(String[] args) {
         System.out.println("A");
+        get();
+    }
+
+    private static void get() {
+
     }
 }
