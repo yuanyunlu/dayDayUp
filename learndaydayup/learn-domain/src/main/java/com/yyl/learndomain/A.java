86ed1f6e852b3566fe54dbacb2282780cf440a50
yuanyunlu <39354624+yuanyunlu@users.noreply.github.com>
2019-05-16 09:45:11 +0800

    Update A.java

--- learndaydayup/learn-domain/src/main/java/com/yyl/learndomain/A.java
+++ learndaydayup/learn-domain/src/main/java/com/yyl/learndomain/A.java
@@ -2,6 +2,6 @@ package com.yyl.learndomain;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("增加main方法111branch1");
+        System.out.println("验证冲突");
     }
 }
